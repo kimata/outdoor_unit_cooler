36cce053a03c677f8f963f6ad7fdc511d2f6cba7
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-06-04 21:58:52 +0900

    fix: 一定時間電磁弁OFFの場合，流量センサの電源を切ったままにするようにした

--- src/unit_cooler.py
+++ src/unit_cooler.py
@@ -21,16 +21,15 @@ with open(str(pathlib.Path(os.path.dirname(__file__), "config.yaml"))) as file:
 state = aircon.get_state("書斎エアコン") or aircon.get_state("和室エアコン")
 duration = valve.set_state(state)
 
-flow = fd_q10c.sense()
-
-logging.info("flow: {flow} L/min".format(flow=flow))
-
 if state:
-    if (duration > 10) and (flow < 0.01):
-        notifier.send(config, "元栓が閉じています．")
+    if duration > 10:
+        flow = fd_q10c.sense()
+        if flow < 0.01:
+            notifier.send(config, "元栓が閉じています．")
 else:
     if duration / (60 * 60) > 1:
         fd_q10c.stop()
-
-    if (duration > 100) and (flow > 0.01):
-        notifier.send(config, "電磁弁が壊れています．")
+    elif duration > 100:
+        flow = fd_q10c.sense()
+        if flow > 0.01:
+            notifier.send(config, "電磁弁が壊れています．")
