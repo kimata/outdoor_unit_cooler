9079ee3d99f787791bf6726d1b612e6ad886ac93
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-06-04 11:03:20 +0900

    fix: 外気温が低い場合は，エアコンが動いていても False を返すようにした

--- src/aircon.py
+++ src/aircon.py
@@ -9,30 +9,40 @@ INFLUXDB_ADDR = "192.168.0.10"
 INFLUXDB_PORT = 8086
 INFLUXDB_DB = "sensor"
 
-INFLUXDB_QUERY = """
+INFLUXDB_POWER_QUERY = """
 SELECT mean("power") FROM "fplug" WHERE ("hostname" = \'{name}\') AND time >= now() - 1h GROUP BY time(5m) fill(previous) ORDER by time desc LIMIT 2
 """
+INFLUXDB_TEMP_QUERY = """
+SELECT mean("temp") FROM "sensor.esp32" WHERE ("hostname" = \'{name}\') AND time >= now() - 1h GROUP BY time(5m) fill(previous) ORDER by time desc LIMIT 2
+"""
 
+TEMP_THRESHOLD = 20
 POWER_THRESHOLD = 50
+
+
+def get_db_value(query):
+    client = InfluxDBClient(
+        host=INFLUXDB_ADDR, port=INFLUXDB_PORT, database=INFLUXDB_DB
+    )
+    result = client.query(query)
+
+    points = list(
+        filter(lambda x: not x is None, map(lambda x: x["mean"], result.get_points()))
+    )
+
+    return points[0]
 
 
 def get_state(name):
     try:
-        client = InfluxDBClient(
-            host=INFLUXDB_ADDR, port=INFLUXDB_PORT, database=INFLUXDB_DB
-        )
-        result = client.query(INFLUXDB_QUERY.format(name=name))
+        power = get_db_value(INFLUXDB_POWER_QUERY.format(name=name))
+        temp = get_db_value(INFLUXDB_TEMP_QUERY.format(name="ESP32-outdoor-1"))
 
-        points = list(
-            filter(
-                lambda x: not x is None, map(lambda x: x["mean"], result.get_points())
-            )
-        )
-        judge = points[0] > POWER_THRESHOLD
+        judge = (power > POWER_THRESHOLD) and (temp > TEMP_THRESHOLD)
 
         logging.info(
-            "{name}: {power:,} W [state: {judge}]".format(
-                name=name, power=round(points[0], 1), judge=judge
+            "{name}: {power:,}W, 外気温: {temp}℃  [state: {judge}]".format(
+                name=name, power=round(power, 1), temp=round(temp, 1), judge=judge
             )
         )
         return judge
