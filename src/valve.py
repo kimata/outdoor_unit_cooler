2128466f94b16530d3a5e37ff44864d136e215f4
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-06-22 10:57:56 +0900

    fix: 制御間隔を縮めた

--- src/valve.py
+++ src/valve.py
@@ -33,8 +33,8 @@ import logging
 
 # NOTE: バルブを ON にする場合，常に ON にするわけではなく，
 # 次の時間(分)毎に ON と OFF を繰り返すようにする
-INTERVAL_MIN_ON = 2
-INTERVAL_MIN_OFF = 2
+INTERVAL_MIN_ON = 1
+INTERVAL_MIN_OFF = 1
 
 GPIO_PIN = 17
 
