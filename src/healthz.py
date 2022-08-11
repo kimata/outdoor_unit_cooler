6b538a34c5360c0833a4e98c292948c60f4078f1
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-08-11 18:42:22 +0900

    feat: Livenss 用のスクリプトを追加

---
+++ src/healthz.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pathlib
+import datetime
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, "lib"))
+
+from config import load_config
+
+config = load_config()
+
+liveness_file = pathlib.Path(config["liveness"]["file"])
+
+if not liveness_file.exists():
+    print("Not executed.", file=sys.stderr)
+    sys.exit(-1)
+
+elapsed = datetime.datetime.now() - datetime.datetime.fromtimestamp(
+    liveness_file.stat().st_mtime
+)
+if elapsed.seconds > config["sense"]["interval"]:
+    print(
+        "Execution interval is too long. ({elapsed:,} sec)".format(
+            elapsed=elapsed.seconds
+        ),
+        file=sys.stderr,
+    )
+    sys.exit(-1)
+
+print("OK.", file=sys.stderr)
+sys.exit(0)
