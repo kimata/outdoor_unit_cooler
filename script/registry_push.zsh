0d8d83fb64cfe4fc2406e88bc0b83c55911acd9d
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-08-13 15:50:55 +0900

    fix: Raspberry Pi 上で実行しているかチェックするようにした

--- script/registry_push.zsh
+++ script/registry_push.zsh
@@ -3,6 +3,11 @@
 NAME=unit_cooler
 REGISTRY=registry.green-rabbit.net/library
 
+if [ $(uname -m) != "aarch64" ]; then
+    echo "Raspberry Pi 上で実行してください．"
+    exit -1
+fi
+
 git push
 docker build --platform linux/arm64 . -t ${NAME}
 docker tag ${NAME} ${REGISTRY}/${NAME}
