104b8ab666d32195d94fb111f7a85e2f41258d50
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-05-27 20:06:31 +0900

    fix: buldx を使うようにした

--- script/registry_push.zsh
+++ script/registry_push.zsh
@@ -3,12 +3,10 @@
 NAME=outdoor_unit_cooler
 REGISTRY=registry.green-rabbit.net:5000/kimata
 
-if [ $(uname -m) != "aarch64" ]; then
-    echo "Raspberry Pi 上で実行してください．"
-    exit -1
-fi
-
 git pull
-docker build --platform linux/arm64 . -t ${NAME}
-docker tag ${NAME} ${REGISTRY}/${NAME}
-docker push ${REGISTRY}/${NAME}
+
+# docker buildx rm arm_builder
+docker buildx create --name arm_builder --config ${0:a:h}/../buildkitd.toml --use
+docker buildx use arm_builder
+docker buildx build --platform linux/amd64,linux/arm64/v8 . --output=type=image,push=true --tag ${REGISTRY}/${NAME}
+
