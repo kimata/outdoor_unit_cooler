a147e2ed24946a2c90ae9924c2c9a43927f4e43c
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-05-27 20:07:29 +0900

    fix: 設定ファイルの場所を変更

--- script/registry_push.zsh
+++ script/registry_push.zsh
@@ -6,7 +6,7 @@ REGISTRY=registry.green-rabbit.net:5000/kimata
 git pull
 
 # docker buildx rm arm_builder
-docker buildx create --name arm_builder --config ${0:a:h}/../buildkitd.toml --use
+docker buildx create --name arm_builder --config ${0:a:h}/buildkitd.toml --use
 docker buildx use arm_builder
 docker buildx build --platform linux/amd64,linux/arm64/v8 . --output=type=image,push=true --tag ${REGISTRY}/${NAME}
 
