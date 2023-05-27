0e7075e7caac14e90d58dfcfd3e893e7d7630ce3
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-05-27 22:07:05 +0900

    fix: Cron で実行した場合も変なパスにならないようにした

--- app/unit_cooler.py
+++ app/unit_cooler.py
@@ -230,7 +230,11 @@ if debug_mode:
 else:
     log_level = logging.INFO
 
-logger.init("hems.unit_cooler", level=log_level, dir_path="log")
+logger.init(
+    "hems.unit_cooler",
+    level=log_level,
+    dir_path=pathlib.Path(os.path.dirname(__file__)) / "log",
+)
 
 logging.info("Using config config: {config_file}".format(config_file=config_file))
 config = load_config(config_file)
