80d8b8cf54e3f57b6542be3d440b1f2e17178e54
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-06-24 19:05:50 +0900

    fix: 整数変換の方法を変更

--- react/src/components/AirConditioner/AirConditioner.js
+++ react/src/components/AirConditioner/AirConditioner.js
@@ -15,7 +15,7 @@ const AirConditioner = ({ isReady, ctrlStat }) => {
         if (value == null) {
             return 0;
         } else {
-            return parseInt(value);
+            return value.toFixed();
         }
     };
 
