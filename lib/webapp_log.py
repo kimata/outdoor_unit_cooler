0e472336f24770b008cafa7fc09402e277a1497c
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-07-08 09:33:40 +0900

    fix: bug

--- lib/webapp_log.py
+++ lib/webapp_log.py
@@ -49,6 +49,8 @@ def init(config_):
     sqlite.commit()
     sqlite.row_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
 
+    should_terminate = False
+
     log_lock = threading.Lock()
     log_queue = Queue()
     log_thread = threading.Thread(target=app_log_worker, args=(log_queue,))
@@ -60,7 +62,7 @@ def term():
     global log_thread
     global should_terminate
 
-    should_terminate = False
+    should_terminate = True
     log_thread.join()
     sqlite.close()
 
@@ -94,6 +96,8 @@ def app_log_impl(message, level):
 
 
 def app_log_worker(log_queue):
+    global should_terminate
+
     while True:
         if not log_queue.empty():
             log = log_queue.get()
