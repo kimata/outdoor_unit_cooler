1b6e9b8093f6512348c02757b8c7bd0f2f167c89
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-08-16 19:36:06 +0900

    fix: テストの安定性を改善

--- app/cooler_controller.py
+++ app/cooler_controller.py
@@ -117,13 +117,6 @@ def start(arg):
     proxy_thread = None
     control_thread = None
     try:
-        control_thread = control_server_start(
-            config,
-            setting["real_port"],
-            setting["dummy_mode"],
-            setting["speedup"],
-            setting["msg_count"],
-        )
         if not setting["disable_proxy"]:
             proxy_thread = cache_proxy_start(
                 config,
@@ -132,6 +125,14 @@ def start(arg):
                 setting["server_port"],
                 setting["msg_count"],
             )
+        control_thread = control_server_start(
+            config,
+            setting["real_port"],
+            setting["dummy_mode"],
+            setting["speedup"],
+            setting["msg_count"],
+        )
+
     except:
         notify_error(config, traceback.format_exc())
         pass
